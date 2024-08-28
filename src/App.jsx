fed6406ff539bb8d363bdd6a829cc14111dcc9a2
saviovnn <savioviannasilva@hotmail.com>
2024-08-28 11:56:03 -0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="bg-gray-800 h-screen flex flex-col items-center text-white">
       <JogoDaVelha />
-      <footer className=" flex flex-1 items-end py-4">
+      <footer className=" flex flex-1 items-end">
         <div className="text-gray-600 text-center font-semibold">
           Savio Vianna - savioviannasilva@hotmail.com
         </div>
